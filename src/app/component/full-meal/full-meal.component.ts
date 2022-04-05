f86eb0da031f74b7cca3d51007c474975ebb3468
RomanMaruk <maruchok2@gmail.com>
2022-04-05 14:03:55 +0300

    change dispatch in full meal

--- src/app/component/full-meal/full-meal.component.ts
+++ src/app/component/full-meal/full-meal.component.ts
@@ -1,7 +1,9 @@
+import { PlatformLocation } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { FullMealInterface } from 'src/app/model/meal/meal.interface';
+import { FullMealAction } from 'src/app/store/meal/meal.action';
 import { MealState } from 'src/app/store/meal/meal.state';
 
 @Component({
@@ -13,10 +15,13 @@ export class FullMealComponent implements OnInit {
   @Select(MealState.fulMeal)
   fullMeal$: Observable<FullMealInterface>
 
-  constructor(private store: Store) { }
+  constructor(private store: Store, private pLocation: PlatformLocation) { }
+
+  index: number = this.pLocation.pathname.lastIndexOf('/')
+  idMeal: number = +this.pLocation.pathname.slice(this.index + 1)
 
   ngOnInit(): void {
-
+    this.store.dispatch(new FullMealAction(this.idMeal))
   }
 
 }
